3d1b73da3ce3bc737750149d80dc51e7652e8d59
Bunny <varun.verma2020@vitstudent.ac.in>
2023-06-16 07:27:43 +0530

    first time using git bash inside VS code

---
+++ Js/Class11.js
@@ -0,0 +1,21 @@
+/* 
+    Agenda : 
+    -------
+    - Intro into JS
+    - Chrome Dev tools
+    - variables 
+    - data types 
+    - types of Operators 
+    - type coehersion
+    - adding JS to html & console logging 
+
+
+
+    JavaScript
+    ----------
+    1. It is a Programming language (unlike html or css)
+    2. It is dynamic typed (do need to declare the data type of a variable). Opposite of static typed languages (eg: Java)
+    3. It is High Level Programming Langauge
+    4. Scripting Language 
+*/
+
