b5a9c9e0e0948ba7b5edb5294e35966b7ca064e3
Bunny <varun.verma2020@vitstudent.ac.in>
2023-06-19 18:53:24 +0530

    added if & else conditional statement

--- Js/Class12.js
+++ Js/Class12.js
@@ -15,3 +15,16 @@
 
 console.log("Installing Node.js latest version...\n(node -v to check)\nInstallation completed.\n\n");
 console.log("To run js file from the terminal using Node.js : node filename.js\n\n")
+
+let age = 28; 
+
+console.log("If else conditional statement :-")
+if(age > 18 ){
+    console.log("Go ahead and perform your right to vote") ;
+}
+else if(age==18){
+    console.log("Go ahead and register yourself as a voter !") ;
+}
+else {
+    console.log("You are not aligible to vote yet") ;
+}
