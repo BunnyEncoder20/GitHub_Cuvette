28247ca8bb757fbd431864b1d2d1203c5c0e6394
Bunny <varun.verma2020@vitstudent.ac.in>
2023-06-19 19:38:52 +0530

    added code for For and Do while loops

--- Js/Class12.js
+++ Js/Class12.js
@@ -36,7 +36,7 @@ console.log("\n\nTernary Operator :-") ;
 age>18 ? console.log("Go ahead and perform your right to vote") : console.log("You are not aligible to vote yet") ;
 
 
-console.log("\n\n While loop in JS \n");
+console.log("\n\n While loop\n");
 let i = 1 ;
 while(i<=10) {
 
@@ -49,3 +49,15 @@ while(i<=10) {
     console.log("14 x ", i, " = " , i*14);
     i++;
 }
+
+console.log("\n\n For loop in JS \n");
+for(let j=1; j<=10 ; j++)
+    console.log(j);
+
+
+// DO while is a exit controlled loop (it will run once (first iteration) without checking the conditional statement)
+console.log("\n\n Do While loop in JS \n");
+let j=10 ;
+do{
+    console.log(j);
+}while (j<10) ; //notice even though j already greater that 10 it still ran once unlike while or for loops
