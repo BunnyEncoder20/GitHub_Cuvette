7530909d7ba2326dd7fbc07e00989730cef885a0
Bunny <varun.verma2020@vitstudent.ac.in>
2023-06-19 18:48:26 +0530

    Started Class12 on JS conditonal statements

---
+++ Js/Class12.js
@@ -0,0 +1,17 @@
+/* 
+    Agenda
+    -------
+    > Conditionals in JS 
+        - if else 
+        - switch 
+
+    > Loops in JS
+        - for 
+        - while 
+        - do while 
+
+    > break & continue 
+*/
+
+console.log("Installing Node.js latest version...\n(node -v to check)\nInstallation completed.\n\n");
+console.log("To run js file from the terminal using Node.js : node filename.js\n\n")
