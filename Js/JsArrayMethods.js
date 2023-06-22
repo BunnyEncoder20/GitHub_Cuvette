6666e5c3a8f3471c831a1ae8cd830cdbbc7e774d
Bunny <varun.verma2020@vitstudent.ac.in>
2023-06-22 14:42:47 +0530

    added notes for map function

--- Js/JsArrayMethods.js
+++ Js/JsArrayMethods.js
@@ -39,6 +39,7 @@ items.forEach( item => {
 
 console.log(count);
 
+console.log("\n\n")
 /* 
     map() method 
      ----------- 
@@ -46,10 +47,61 @@ console.log(count);
 const numbers = [1,2,3,4,5] ;
 
 // remember map doesn't change the original array
-// just returns a new array as a result 
-
-numbers.map(double)
-
+// just returns a new array as a result. This first block does nothing 
+numbers.map(double);
 function double(value, index, array) {
-    
+    return value*2 ;
 }
+console.log(numbers);
+
+var newnumbers = numbers.map(double) ;
+console.log(newnumbers);
+
+// can also be done using arrow fucntions (function expression)
+newnumbers = numbers.map(value => value*2) ;
+console.log(newnumbers)
+
+// this is a array of objects 
+const logbook = [
+    {
+        category: 'laptops',
+        price : 7000,
+        inStock : 20 
+    },
+    {
+        category: 'mobiles',
+        price : 500,
+        inStock : 50 
+    },
+    {
+        category: 'desktops',
+        price : 10000,
+        inStock : 5 
+    }
+];
+
+var shopEvaluation = logbook.map(item => {
+    return item.price * item.inStock ;
+});
+console.log(shopEvaluation);
+
+// How to return an array object as 
+shopEvaluation = logbook.map(item => (
+    {
+        Category : item.category,
+        Cost : item.price * item.inStock 
+    }
+));
+console.log(shopEvaluation);
+
+
+// Converting a array of string numbers into array of numbers 
+const str = ['1','2','3','4','5'] ;
+var nums = str.map(item => {
+    return Number(item);
+}) ;
+console.log(nums);
+
+// alternative 
+nums = str.map(item => Number(item)) ;
+console.log(nums);
