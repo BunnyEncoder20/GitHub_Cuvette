ead46101930cbe7edcfd9fb16dab05acd252e52d
Bunny <varun.verma2020@vitstudent.ac.in>
2023-07-11 12:45:35 +0530

    finsihed the DOM manipulation, moving onto events and event handlers

--- Js/DOM.js
+++ Js/DOM.js
@@ -19,3 +19,17 @@ htmlCollection1[0].innerHTML = "<u>Changed using JS</u>"
 // Changing the stlye of DOM elements 
 let htmlCollection2 = document.getElementsByTagName('h1');
 htmlCollection2[0].style.color = 'violet' ;
+
+
+// Creating a DOM element 
+let buttonElement = document.createElement('button') ;
+buttonElement.innerText = "Click to Submit" ; 
+
+// Adding attribute to the elements 
+let imageElement  = document.createElement('img');
+imageElement.setAttribute('src','../HTML/Images/catgiggle.gif') 
+imageElement.setAttribute('alt','Image Added using created DOM element')
+
+// adding the created DOM element to the document 
+document.body.appendChild(buttonElement) ;
+document.body.appendChild(imageElement) ;
