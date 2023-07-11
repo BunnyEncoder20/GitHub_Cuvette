ef8a834d26e973e478da3f43daf22f3fc0a851d3
Bunny <varun.verma2020@vitstudent.ac.in>
2023-07-11 09:24:27 +0530

    Part1 - accessing Document elements completed

---
+++ Js/DOM.js
@@ -0,0 +1,12 @@
+/*
+    DOM Manipulation
+    ----------------
+
+    Remember that : 
+    -> document.getElementByID() returns one unique html element
+    -> document.getElementsByClassName() returns an htmlCollection
+        (an array of elements), in which  the actual html elements are 
+        accessible using index
+    -> document.getElementByTagName() also behaves same as ClassName()
+    
+*/
