091d594773cba05666bb344d49d0be776db4a309
Bunny <varun.verma2020@vitstudent.ac.in>
2023-07-24 21:45:20 +0530

    added notes for event bubbling and .stopPropagation()

--- Js/DOM.js
+++ Js/DOM.js
@@ -36,7 +36,7 @@ document.body.appendChild(buttonElement) ;
 document.body.appendChild(imageElement) ;
 
 // Events in JS
-eventBtn = document.getElementById('eventbtn') ;
+let eventBtn = document.getElementById('eventbtn') ;
 eventBtn.onclick = function clickedEvent() {
     console.log('Event broughyt to you by DOM.js') ;
 } ;
@@ -45,3 +45,53 @@ function changeHeading() {
     document.getElementById('eventsHeading').innerHTML = '<h3 id="eventsHeading"><u><b>Events in JS in Bold & Underline</b></u></h3>'
 }
 
+
+// Adding Events listeners 
+eventBtn.addEventListener('dblclick',changeBack) //Note that the 2nd Parameter is a callback function name (hence no () after the function name)
+function changeBack() {
+    document.getElementById('eventsHeading').innerHTML = '<h3 id="eventsHeading">Events in JS </h3>' ;
+}
+
+// PointerEvent Object 
+let pointerEvent = document.getElementById('PointerEventbtn') ;
+pointerEvent.addEventListener('click',printPointerEvent) ;  //remember that the event object is sent by default to the callback fucntion of the eventListener
+
+function printPointerEvent(event) {
+    console.log(event) ;
+} ;
+
+
+/*
+    Event Bubbling
+    -------------
+    > occurs when an child and parent are listening for the same events
+    > eg: when the parent is also listening for a click and child inside that parent is also listening for a click
+    > Then if we want to click for only the button, the event will propogate to the parent also
+
+    > To avoid this situation, we can use the eventObject.stopPropagation()
+    > An silent parameter (event object) is passed to the callback function when event is heard
+    > Eg :
+ */
+let parentElement = document.getElementById("parentElement") ;
+let childElement1 = document.getElementById("childElement1") ;
+let childElement2 = document.getElementById("childElement2") ;
+
+parentElement.addEventListener('click',parentListener) ;
+childElement1.addEventListener('click',childListener1) ;
+childElement2.addEventListener('click',childListener2) ;
+
+function parentListener (event) {
+    console.log("Parent Listener activated") ;
+    document.getElementById("divHeading").innerText="Change brought you by parentListener()"
+}
+
+function childListener1 (event) {
+    console.log("Child Listener 1 activated") ;
+    childElement1.innerText = "Change Brought to you by childListener1()"
+}
+
+function childListener2 ( event ) {
+    console.log("Child Listener 2 activated") ;
+    childElement2.innerText = "Notice that the Heading did not change this time !"
+    event.stopPropagation() ;
+}
