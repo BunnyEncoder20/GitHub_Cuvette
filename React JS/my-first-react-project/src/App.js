dbcdc8fb7ebe496a3aedfdead2c2d7ef955b56d2
Bunny <varun.verma2020@vitstudent.ac.in>
2023-08-01 18:50:39 +0530

    completed Arrays and their mapping to components

--- React JS/my-first-react-project/src/App.js
+++ React JS/my-first-react-project/src/App.js
@@ -3,6 +3,28 @@ import Footer from './Footer'
 import Card from './Card'
 
 function App() {
+
+  let arr = [1999,700,500] ;
+  arr.map((item)=>{
+    return console.log(item) ;
+  })
+
+  let arr2D = [[1,2,3],[4,5,6]] ;
+  arr2D.map((arr)=>{
+      return arr.map((subitems)=>{
+        console.log(subitems);
+      })
+  })
+
+  let productArray = [
+    {id:1001,title:"pencil",price:10},
+    {id:1002,title:"notebook",price:50},
+    {id:1003,title:"Apple",price:100},
+  ] ;
+  productArray.map((obj)=>{
+    return console.log(obj.id) ;
+  })
+
   return (
     <>
       <Header/>
@@ -12,6 +34,16 @@ function App() {
       <Card title={"Tailored Jeans"} price={1999} />
       <Card title={"Shoes"} price={700} />
       <Card price={500} />
+
+      <br/><br/>
+      <h1><u>Class 4 Content</u></h1>
+      
+      {
+        productArray.map((product)=>(
+          <Card key={product.id} title={product.title} price={product.price} />
+        ))
+      }
+
     </>
   );
 }
