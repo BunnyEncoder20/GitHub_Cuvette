3e3d039b51d32f90aee2e863ff07762baa443346
Bunny <varun.verma2020@vitstudent.ac.in>
2023-08-08 08:28:57 +0530

    completed class 10, learned about packages, axios, css modules

--- React JS/my-2nd-react-app/src/Pages/AboutPage.jsx
+++ React JS/my-2nd-react-app/src/Pages/AboutPage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import aboutpagecss from './about.module.css'
 
 function AboutPage() {
   return (
-    <>
-      <div>I'm a AboutPage</div>
-      <Link to="/">Click to go to HomePage</Link>
-    </>
+    <div className={aboutpagecss.container}>
+      <h4>I'm a AboutPage</h4>
+      <Link to="/">Click to go to HomePage</Link><br />
+      { moment().format() }
+      <br /><br />
+    </div>
   )
 }
 
--- React JS/my-2nd-react-app/src/Pages/HomePage.jsx
+++ React JS/my-2nd-react-app/src/Pages/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import homepagecss from './home.module.css'
 
 function HomePage() {
   return (
-    <>
-      <div>I'm a HomePage</div>
-      <Link to="/about">Click to go to AboutPage</Link>
-    </>
+    <div className={homepagecss.container}>
+      <h4>I'm a HomePage</h4>
+      <Link to="/about">Click to go to AboutPage</Link><br />
+      { moment().format() }
+      <br /><br />
+    </div>
   )
 }
 
