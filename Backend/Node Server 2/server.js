2409f0b430607abac4bd9cf179ba6aa2337582ef
Bunny <varun.verma2020@vitstudent.ac.in>
2023-08-29 21:46:54 +0530

    learnt how to make dynamic pages in express

--- Backend/Node Server 2/server.js
+++ Backend/Node Server 2/server.js
@@ -1,6 +1,9 @@
 const express  = require('express');
 const app = express()
 
+const ejs = require('ejs')          // importing ejs 
+app.set('view engine' , 'ejs' )   // setting the template to ejs 
+
 app.listen(3000, ()=>{
     console.log("Server running on http://localhost:3000")
 })
@@ -11,7 +14,7 @@ const dummyUsersDB = [
         "email": "george.bluth@reqres.in",
         "first_name": "George",
         "last_name": "Bluth",
-        //"avatar": "https://reqres.in/img/faces/1-image.jpg"
+        "avatar": "https://reqres.in/img/faces/1-image.jpg"
       },
       {
         "id": 2,
@@ -50,7 +53,51 @@ const dummyUsersDB = [
       }
 ]
 
-app.get('/soma' , (req,res) => {
-    res.sendFile(__dirname+'/profile.html')
+// app.get('/soma' , (req,res) => {
+//     res.sendFile(__dirname+'/profile.html')
+// })
+
+app.get('/temp' , (req,res) => {
+  res.render('temp')    // when using ejs render, server knows that it is gonna take the files from the view folder itself. Hence it is recommended to make a view folder 
 })
 
+app.get('/Topic1' , (req,res) => {
+  res.render('temp' , { heading: 'Soma' } )    // sending object of variables and their values to the ejs page 
+})
+app.get('/Topic2' , (req,res) => {
+  res.render('temp' , { heading: 'Senpai' })
+})
+app.get('/Topic3' , (req,res) => {
+  res.render('temp' , { heading: 'Soma Senpai' })
+})
+
+app.get('/somya' , (req,res) => {
+  res.render('profileTemplate' , {
+      "id": 1,
+      "email": "somyaKumari@gmail.com",
+      "first_name": "Somya",
+      "last_name": "Kumari",
+      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKMxDoUg9xG2fuWCnRDuCc3TfbAOhc7YhWw&usqp=CAU"
+    }
+  )
+})  
+app.get('/soma' , (req,res) => {
+  res.render('profileTemplate' , {
+      "id": 2,
+      "email": "soma@gmail.com",
+      "first_name": "Soma",
+      "last_name": "",
+      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRcqu4xZu-j4C0988_Mithjj8EenYPTI7aog&usqp=CAU"
+    }
+  )
+})  
+app.get('/senpai' , (req,res) => {
+  res.render('profileTemplate' , {
+      "id": 3,
+      "email": "senpai@gmail.com",
+      "first_name": "Senpai",
+      "last_name": "",
+      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2sjxTW9GqBq8VOEbKHX0Tsr-4-jl8Cfmsyg&usqp=CAU"
+    }
+  )
+})  
