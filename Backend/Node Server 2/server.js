6e6b780c421efb3bba410d82103c1b8f308bae04
Bunny <varun.verma2020@vitstudent.ac.in>
2023-08-30 14:17:41 +0530

    Finished All the  topics of class 2 (live one)

--- Backend/Node Server 2/server.js
+++ Backend/Node Server 2/server.js
@@ -14,48 +14,58 @@ const dummyUsersDB = [
         "email": "george.bluth@reqres.in",
         "first_name": "George",
         "last_name": "Bluth",
-        "avatar": "https://reqres.in/img/faces/1-image.jpg"
+        "avatar": "https://reqres.in/img/faces/1-image.jpg",
+        "likescake" : true
       },
       {
         "id": 2,
         "email": "janet.weaver@reqres.in",
         "first_name": "Janet",
         "last_name": "Weaver",
-        //"avatar": "https://reqres.in/img/faces/2-image.jpg"
+        "avatar": "https://reqres.in/img/faces/2-image.jpg",
+        "likescake" : false
       },
       {
         "id": 3,
         "email": "emma.wong@reqres.in",
         "first_name": "Emma",
         "last_name": "Wong",
-        //"avatar": "https://reqres.in/img/faces/3-image.jpg"
+        "avatar": "https://reqres.in/img/faces/3-image.jpg",
+        "likescake" : true
       },
       {
         "id": 4,
         "email": "eve.holt@reqres.in",
         "first_name": "Eve",
         "last_name": "Holt",
-        //"avatar": "https://reqres.in/img/faces/4-image.jpg"
+        "avatar": "https://reqres.in/img/faces/4-image.jpg",
+        "likescake" : true
       },
       {
         "id": 5,
         "email": "charles.morris@reqres.in",
         "first_name": "Charles",
         "last_name": "Morris",
-        //"avatar": "https://reqres.in/img/faces/5-image.jpg"
+        "avatar": "https://reqres.in/img/faces/5-image.jpg",
+        "likescake" : false
       },
       {
         "id": 6,
         "email": "tracey.ramos@reqres.in",
         "first_name": "Tracey",
         "last_name": "Ramos",
-        //"avatar": "https://reqres.in/img/faces/6-image.jpg"
+        "avatar": "https://reqres.in/img/faces/6-image.jpg",
+        "likescake" : false
       }
 ]
 
 // app.get('/soma' , (req,res) => {
 //     res.sendFile(__dirname+'/profile.html')
 // })
+
+app.get('/',(req,res)=>{
+  res.send('Server Up and Running')
+})
 
 app.get('/temp' , (req,res) => {
   res.render('temp')    // when using ejs render, server knows that it is gonna take the files from the view folder itself. Hence it is recommended to make a view folder 
@@ -77,7 +87,8 @@ app.get('/somya' , (req,res) => {
       "email": "somyaKumari@gmail.com",
       "first_name": "Somya",
       "last_name": "Kumari",
-      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKMxDoUg9xG2fuWCnRDuCc3TfbAOhc7YhWw&usqp=CAU"
+      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTeKMxDoUg9xG2fuWCnRDuCc3TfbAOhc7YhWw&usqp=CAU",
+      "likescake" : false
     }
   )
 })  
@@ -87,7 +98,8 @@ app.get('/soma' , (req,res) => {
       "email": "soma@gmail.com",
       "first_name": "Soma",
       "last_name": "",
-      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRcqu4xZu-j4C0988_Mithjj8EenYPTI7aog&usqp=CAU"
+      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJhoTgRLmdkSG3r4xRH34ia_voV__Zjiaw0Q&usqp=CAU",
+      "likescake" : true
     }
   )
 })  
@@ -97,7 +109,63 @@ app.get('/senpai' , (req,res) => {
       "email": "senpai@gmail.com",
       "first_name": "Senpai",
       "last_name": "",
-      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2sjxTW9GqBq8VOEbKHX0Tsr-4-jl8Cfmsyg&usqp=CAU"
+      "avatar": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2sjxTW9GqBq8VOEbKHX0Tsr-4-jl8Cfmsyg&usqp=CAU",
+      "likescake" : true
     }
   )
 })  
+app.get('/George' , (req,res)=>{
+  let details = dummyUsersDB.find(user=>user.first_name === "George");
+  console.log(details);
+  res.render('profileTemplate', details)
+})
+app.get('/Janet' , (req,res)=>{
+  let details = dummyUsersDB.find(user=>user.first_name === "Janet");
+  console.log(details);
+  res.render('profileTemplate', details)
+})
+app.get('/Emma' , (req,res)=>{
+  let details = dummyUsersDB.find(user=>user.first_name === "Emma");
+  console.log(details);
+  res.render('profileTemplate', details)
+})
+app.get('/Eve' , (req,res)=>{
+  let details = dummyUsersDB.find(user=>user.first_name === "Eve");
+  console.log(details);
+  res.render('profileTemplate', details)
+})
+app.get('/Charles' , (req,res)=>{
+  let details = dummyUsersDB.find(user=>user.first_name === "Charles");
+  console.log(details);
+  res.render('profileTemplate', details)
+})
+app.get('/Tracey' , (req,res)=>{
+  let details = dummyUsersDB.find(user=>user.first_name === "Tracey");
+  console.log(details);
+  res.render('profileTemplate', details)
+})
+
+
+
+
+// Request Parameters 
+app.get('/example/:parameter' , (req,res) => {
+  console.log(req.params.parameter)
+  res.json({value:req.params.parameter})
+})
+
+// Making all the user fucntions into a single function : 
+app.get('/profiles/:username',(req,res)=>{
+  let username = req.params.username ;
+  // console.log(username)
+  let userdetails = dummyUsersDB.find(user=>user.first_name === username);
+  
+  if (userdetails) {
+    console.log(userdetails);
+    res.render('profileTemplate', userdetails)
+  }
+  else {
+    console.log('User was not found')
+    res.render('notFound')
+  }
+})
