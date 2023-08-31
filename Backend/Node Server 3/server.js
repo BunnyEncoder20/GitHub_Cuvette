2d2e2ff4046239e4924497e821670c59c97fa07b
Bunny <varun.verma2020@vitstudent.ac.in>
2023-08-31 16:26:10 +0530

    Completed Class 3 on MongoDB and Mongoose

---
+++ Backend/Node Server 3/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+const ejs = require('ejs')
+app.set('view engine' , 'ejs')
+
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+// These 2 middleware are important for the connection 
+// remember that app.use() is used to attach middlewares (entity, which lies between client and server)
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static('./public'))
+
+app.listen(process.env.port , ()=>{
+    mongoose
+        .connect(process.env.mongodb_url)
+        .then(()=> console.log("[Server] Sevre is now Running \n[Server] Connection to DB established"))
+        .catch((error)=>console.log(error))
+})
+
+app.get('/',(req,res)=>{
+    res.send('Sever is Up and Runnig on localhost:3000')
+})
+
+// usecase of express.static : 
+app.get('/download',(req,res)=>{
+    // res.download(__dirname+'/public/chickn nuggit.jpeg')  // default way to mention a the path 
+    res.download('public/chickn nuggit.jpeg')
+})
