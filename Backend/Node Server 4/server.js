3428e01549ef64b948144b9d2386d181ea2ead2b
Bunny <varun.verma2020@vitstudent.ac.in>
2023-09-01 07:48:52 +0530

    done half way with class 5

--- Backend/Node Server 4/server.js
+++ Backend/Node Server 4/server.js
@@ -1,2 +1,43 @@
 // npm init
 // npm i express ejs body-parser dotenv mongoose
+
+const express = require('express')
+const app = express()
+
+const ejs = require('ejs')
+app.set('view engine' , 'ejs')
+
+const dotenv = require('dotenv')
+dotenv.config()
+
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+
+// Setting up the middleware
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static('./public'))
+
+// Setting up to listen to requests 
+app.listen(process.env.PORT , ()=>{
+    mongoose
+        .connect(process.env.MONGODB_URL)
+        .then(()=>console.log(`[Server] Server Running at http://localhost:${process.env.PORT}\n[MongoDB] Connected to DB`))
+        .catch((err)=>console.log(err))
+})
+
+
+// Code to create a collection cutomers with basic structure 
+const Customer = mongoose.model('Customer' , {  //note that the actual collection name would be all small letters with a 's' (to make it pural)
+    name : String ,
+    email: String ,
+    phoneNumber:Number,
+    avatarURL:String
+})
+
+app.get('/' , (req,res)=>{
+    res.json({
+        status : "SUCCESS",
+        message : "Server running now"
+    })
+})
