efb11600597d93d5945b96b58cfba273ccc42aff
Bunny <varun.verma2020@vitstudent.ac.in>
2023-12-17 13:16:04 +0530

    completed revision for Classes in JS, added some extra notes

--- Js/Class20 - Classes.js
+++ Js/Class20 - Classes.js
@@ -10,35 +10,44 @@
 /* 
     Classes : 
     ---------
-    > They are like a blurprint of an object 
+    > They are like a blueprint of an object 
     > They are not very powerful in JS (unlike Java or C++)
-    > They can be seen as another way of writting a function. As they way of syntax are kinda similar 
+    > They can be seen as another way of writing a function. As they way of syntax are kinda similar 
     > Eg : 
 */
 
-class Person {
-    // convention to write the class name with a Captial letter 
+// Form 1 : declaration
+class Person1 {
+    // convention to write the class name with a Capital letter 
     name
     constructor(name) {
         this.name = name ;
     }
 }
 
-// Way to access the class methods : 
-const person1 = new Person("Soma") ;
-console.log(person1.name);
+// Form 2 : expression
+const Person2 = class {
+    name
+    constructor(name){
+        this.name=name;
+    }
+}
 
-const person2 = new Person ("Bunny") ;
-console.log(person2.name);
+// Way to access the class methods : create a instance of the class
+const p1 = new Person1("Soma") ;
+console.log(p1.name);
+
+const p2 = new Person2("Bunny") ;
+console.log(p2.name);
 
 /*
     > Remember that classes are not hoisted at all
-    > Hence we cannot acces the, before initialization 
+    > Hence we cannot access them, before initialization 
     
-    > This is how you inherit the properties from another class 
+    > This is how you inherit the properties from another class :
 */
 
-class Teacher extends Person {
+class Teacher extends Person2 {
     constructor(name) {
         super(name);    // note that super refers to the constructor of the parent class
     }
