c698e282f2dc3a2a4bb8c54a0fdba5c09cd8b127
Bunny <varun.verma2020@vitstudent.ac.in>
2023-12-17 13:05:06 +0530

    completed revision of Objects and Prototypes, added some more notes

--- Js/Class19 - Objects and Prototypes.js
+++ Js/Class19 - Objects and Prototypes.js
@@ -2,7 +2,7 @@
  *      Agenda
  * -----------
  *  1. Objects 
- *  2. adding behaviours to objects 
+ *  2. adding behaviors to objects (methods/functions)
  *  3. functions to create object 
  *  4. Prototypes : What and Why ?
  *  5. Properties around prototypes
@@ -28,18 +28,26 @@ let person2 = Object.create ({
     }
 })
 
-console.log(person2)
+console.log(person2)    // the object properties is created under prototype which visible when you run this in the browser command window
 
 /*
-    Prototypes are the way an object inherits properties from it 's parent object 
-    (Remember that everything in JS is an object)
-    Prototype contains the inheritted property of an object and refers back to the parent object
+    > Prototypes are the way an object inherits properties from it's parent object
+    > (Remember that everything in JS is an object)
+    > Prototype contains the inherited property of an object and refers back to the parent object
+    > When a object is created it is inheriting a lot of additional functions (visible in browser console) from 
+        it's Parent object. This inheritance is called prototype inheritance 
 
     > NOTE : 
     > The difference between the 2 ways of creating an object :
     > The first one adds the properties directly to the object 
     > Second one adds the properties to the prototype of that object 
     > hence we can still access the properties even though, they are not shown in the console log
+
+    > __proto__ is reference back to parent object from which we are inheriting the properties
+    > will be null for the Largest parent object. Will contain the properties of the parent if any
+    > when a object doesn't find a property name within itself, it'll check for it in it's prototype, that's how 
+        we are able to see the below peron2.name
+
 */
 
 console.log(person2.name);
