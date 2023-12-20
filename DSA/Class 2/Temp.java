d5895b36b5d7259cf933bd57262568a12fafa21c
Bunny <varun.verma2020@vitstudent.ac.in>
2023-12-20 16:51:38 +0530

    studying backend

--- DSA/Class 2/Temp.java
+++ DSA/Class 2/Temp.java
@@ -1,9 +1,10 @@
 public class Temp {
     public static void main(String[] args) {
-        int a =10, b=13;
-        if(13<=(a+b)&&(a+b)<=19)
-            System.out.println(19);
-        else
-            System.out.println(a+b);
+        String str = "abcxx";
+        for (int i=0;i<str.length()-1;i++){
+            System.out.println(str.substring(i,i+2));
+              System.out.println("i="+i+"\ni+2="+(i+2));
+              System.out.println();
+          }
     }
 }
